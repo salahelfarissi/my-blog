a45126ead5f61bc0d8cc78990a83b096382fe79c
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-11-02 17:54:33 +0100

    Route to articles using useParams hook

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,5 +1,19 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlePage = () => {
-  return <h1>This is the article page!</h1>;
+  const { articleId } = useParams();
+
+  const article = articles.find((article) => article.name === articleId);
+
+  return (
+    <>
+      <h1>{article?.title}</h1>
+      {article?.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+    </>
+  );
 };
 
 export default ArticlePage;
