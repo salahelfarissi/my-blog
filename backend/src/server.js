64ac8fe39956e23ed6c683af4bdc61119a274019
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-11-06 11:13:34 +0100

    Add upvote endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,32 @@
 import express from "express";
 
-const app = express();
+let articlesInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  {
+    name: "mongodb",
+    upvotes: 0,
+  },
+];
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
+const app = express();
+app.use(express.json());
+
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find((article) => article.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.status(200).send(`${name} now has ${article.upvotes} upvotes`);
+  } else {
+    res.status(404).send("Article not found");
+  }
 });
 
 app.listen(8000, () => {
