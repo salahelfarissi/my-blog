3f8254da82c853351a438699b6058a2aecc7aebf
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-11-06 12:10:36 +0100

    Add comments endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,17 @@ let articlesInfo = [
   {
     name: "learn-react",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "learn-node",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "mongodb",
     upvotes: 0,
+    comments: [],
   },
 ];
 
@@ -24,6 +27,18 @@ app.put("/api/articles/:name/upvote", (req, res) => {
   if (article) {
     article.upvotes += 1;
     res.status(200).send(`${name} now has ${article.upvotes} upvotes`);
+  } else {
+    res.status(404).send("Article not found");
+  }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { postedBy, text } = req.body;
+  const { name } = req.params;
+  const article = articlesInfo.find((article) => article.name === name);
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.status(200).send(article.comments);
   } else {
     res.status(404).send("Article not found");
   }
