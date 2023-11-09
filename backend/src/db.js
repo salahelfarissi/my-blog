9df32475849e6dfe14af74a6f34febdd4bd82ffd
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-11-09 13:01:44 +0100

    Add MongoDB database connection and refactor server.js to use it.

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+import { MongoClient } from "mongodb";
+
+let db;
+
+async function connectToDb(callback) {
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+  db = client.db("react-blog-db");
+  callback();
+}
+
+export { connectToDb, db };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+import { db, connectToDb } from "./db.js";
 
 const app = express();
 app.use(express.json());
@@ -7,11 +7,6 @@ app.use(express.json());
 // TODO: Use /api prefix for all routes
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
-
-  const client = new MongoClient("mongodb://127.0.0.1:27017");
-  await client.connect();
-
-  const db = client.db("react-blog-db");
 
   const article = await db.collection("articles").findOne({ name });
 
@@ -24,11 +19,6 @@ app.get("/api/articles/:name", async (req, res) => {
 
 app.put("/api/articles/:name/upvote", async (req, res) => {
   const { name } = req.params;
-
-  const client = new MongoClient("mongodb://127.0.0.1:27017");
-  await client.connect();
-
-  const db = client.db("react-blog-db");
 
   const article = await db.collection("articles").findOne({ name });
 
@@ -52,11 +42,6 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   const { postedBy, text } = req.body;
   const { name } = req.params;
 
-  const client = new MongoClient("mongodb://127.0.0.1:27017");
-  await client.connect();
-
-  const db = client.db("react-blog-db");
-
   const article = await db.collection("articles").findOne({ name });
 
   if (article) {
@@ -78,6 +63,8 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   }
 });
 
-app.listen(8000, () => {
-  console.log("Server listening on port 8000");
+connectToDb(() => {
+  app.listen(8000, () => {
+    console.log("Server listening on port 8000");
+  });
 });
