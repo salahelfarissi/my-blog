d9fc5bef0f3f2214531f84697d18cb968aee7cb1
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-11-09 16:28:05 +0100

    Add axios package and fetch article info from backend API

--- frontend/src/pages/Article.jsx
+++ frontend/src/pages/Article.jsx
@@ -1,9 +1,24 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFound";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(`/api/articles/${articleId}`);
+      const body = result.data;
+
+      setArticleInfo(body);
+    };
+
+    fetchData();
+  }, [articleId]);
+
   const article = articles.find((article) => article.name === articleId);
 
   if (!article) return <NotFoundPage />;
@@ -11,6 +26,7 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article?.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s).</p>
       {/* ? key arg corresponds to index */}
       {/* Don't use index if the list items will be added to or removed */}
       {article?.content.map((paragraph, key) => (
