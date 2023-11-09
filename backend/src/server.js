bf1b98f3880b3ac2b5bac6f1025095021c06dadb
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-11-09 12:41:16 +0100

    Add MongoDB integration for upvoting articles

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,14 +22,27 @@ app.get("/api/articles/:name", async (req, res) => {
   }
 });
 
-app.put("/api/articles/:name/upvote", (req, res) => {
+app.put("/api/articles/:name/upvote", async (req, res) => {
   const { name } = req.params;
-  const article = articlesInfo.find((article) => article.name === name);
+
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+
+  const db = client.db("react-blog-db");
+
+  const article = await db.collection("articles").findOne({ name });
+
   if (article) {
-    article.upvotes += 1;
-    res.status(200).send(`${name} now has ${article.upvotes} upvotes`);
-  } else {
-    res.status(404).send("Article not found");
+    await db.collection("articles").updateOne(
+      { name },
+      {
+        $inc: {
+          upvotes: 1,
+        },
+      }
+    );
+    const updatedArticle = await db.collection("articles").findOne({ name });
+    res.status(200).send(`${name} now has ${updatedArticle.upvotes} upvotes`);
   }
 });
 
