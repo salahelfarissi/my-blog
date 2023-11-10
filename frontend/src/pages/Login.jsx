fdd25e63c5b166a3f0d2f8241d4bdf65e3a5edc2
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-11-10 16:37:18 +0100

    Add login functionality to LoginPage component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 const LoginPage = () => {
-  return <h1>Log In</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const logIn = async (e) => {
+    e.preventDefault();
+    setError("");
+    const auth = getAuth();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/articles");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <>
+      <h1>Log In</h1>
+      {error && <p className="error">{error}</p>}
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={logIn}>Log In</button>
+      <Link to="/create-account">Don't have an account? Create one here</Link>
+    </>
+  );
 };
 
 export default LoginPage;
