712966f532e2e965d6b102891160bc7f8a2182ec
Salaheddine EL FARISSI <salaheddine.elfarissi@gmail.com>
2023-11-10 20:16:56 +0100

    Add Firebase authentication and upvote functionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,38 +1,77 @@
+import fs from "fs";
 import express from "express";
 import { db, connectToDb } from "./db.js";
+import admin from "firebase-admin";
+
+const credentials = JSON.parse(fs.readFileSync("../credentials.json"));
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
+});
 
 const app = express();
 app.use(express.json());
 
+app.use(async (req, res, next) => {
+  const { authToken } = req.headers;
+  if (authToken) {
+    try {
+      req.user = await admin.auth().verifyIdToken(authToken);
+    } catch (error) {
+      res.sendStatus(400);
+    }
+  }
+  next();
+});
+
 // TODO: Use /api prefix for all routes
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
+  const { uid } = req.user;
 
   const article = await db.collection("articles").findOne({ name });
 
   if (article) {
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpvote = uid && !upvoteIds.includes(uid);
     res.status(200).json(article);
   } else {
     res.status(404).send("Article not found");
   }
 });
 
+app.use((req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.status(401).send("Unauthorised");
+  }
+});
+
 app.put("/api/articles/:name/upvote", async (req, res) => {
   const { name } = req.params;
+  const { uid } = req.user;
 
   const article = await db.collection("articles").findOne({ name });
 
   if (article) {
-    await db.collection("articles").updateOne(
-      { name },
-      {
-        $inc: {
-          upvotes: 1,
-        },
-      }
-    );
+    const upvoteIds = article.upvoteIds || [];
+    const canUpvote = uid && !upvoteIds.includes(uid);
+
+    if (canUpvote) {
+      await db.collection("articles").updateOne(
+        { name },
+        {
+          $inc: {
+            upvotes: 1,
+          },
+          $push: {
+            upvoteIds: uid,
+          },
+        }
+      );
+    }
     const updatedArticle = await db.collection("articles").findOne({ name });
-    // Send the updated article object as response
     res.status(200).json(updatedArticle);
   } else {
     res.status(404).send("Article not found");
@@ -40,8 +79,9 @@ app.put("/api/articles/:name/upvote", async (req, res) => {
 });
 
 app.post("/api/articles/:name/comments", async (req, res) => {
-  const { postedBy, text } = req.body;
+  const { text } = req.body;
   const { name } = req.params;
+  const { email } = req.user;
 
   const article = await db.collection("articles").findOne({ name });
 
@@ -51,7 +91,7 @@ app.post("/api/articles/:name/comments", async (req, res) => {
       {
         $push: {
           comments: {
-            postedBy,
+            postedBy: email,
             text,
           },
         },
